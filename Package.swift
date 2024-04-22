fd8cf8af2e77930e4bec716b1a80b1ed091cf081
johnnyrpy <johnny@reepay.com>
2024-04-22 13:22:46 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Reepay",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        .library(
+            name: "ReepayCheckoutSheet",
+            targets: ["ReepayCheckoutSheet"]),
+    ],
+    dependencies: [
+        .package(name: "Reepay", url: "https://github.com/reepay/reepay-ios.git", from: "1.0.0"),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ReepayCheckoutSheet",
+            url: "https://github.com/reepay/reepay-ios/releases/download/1.0.0/Reepay.xcframework.zip",
+            checksum: "109e9661c16691649c005ed1190f54a19c230f93beeff5c0c5569e332356dce1"
+        ),
+    ])
